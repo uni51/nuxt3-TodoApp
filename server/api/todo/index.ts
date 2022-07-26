59b5bf0865f3b8fe32fb132a86119477740f83b1
Koichi.Shimomura <unieye51@gmail.com>
2022-07-26 19:21:04 +0900

    9-55 GET Requests

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,3 +1,11 @@
-export default defineEventHandler(() => {
-	return "Hello this is data";
+import { db } from "../../db";
+
+export default defineEventHandler((event) => {
+	console.log({ event });
+	const method = event.req.method;
+
+	if (method === "GET") {
+		return db.todos;
+	}
+
 });
