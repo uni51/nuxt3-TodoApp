4aa826dab27f66b59515807fa3357c12e380debe
Koichi.Shimomura <unieye51@gmail.com>
2022-07-26 19:35:54 +0900

    9-56 POST Requests and Body

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,11 +1,29 @@
 import { db } from "../../db";
+import { v4 as uuid } from "uuid";
 
-export default defineEventHandler((event) => {
-	console.log({ event });
-	const method = event.req.method;
+export default defineEventHandler(async (e) => {
+	console.log({ e });
+	const method = e.req.method;
 
 	if (method === "GET") {
 		return db.todos;
 	}
+
+	if (method === "POST") {
+		const body = await useBody(e);
+
+    if (!body.item) throw new Error()
+		
+		const newTodo = {
+			id: uuid(),
+			item: body.item,
+			completed: false,
+		};
+
+		db.todos.push(newTodo);
+
+		return newTodo;
+	}
+	
 
 });
